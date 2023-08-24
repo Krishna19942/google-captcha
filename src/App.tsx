619b16582c6f59fa66bc5e41f9c55f75ed1c1011
Ramdin Sharvesh <ramdin.s@pg.com>
2023-08-24 12:02:18 +0400

    add button

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+import { GoogleReCaptchaProvider, GoogleReCaptcha } from 'react-google-recaptcha-v3';
 function App() {
   return (
     <div className="App">
       <GoogleReCaptchaProvider reCaptchaKey="6Lf_u84nAAAAAAnNuPWpkfjdbRT3dLi28Nt-zMuz">
+        <GoogleReCaptcha onVerify={()=> {console.log('true')}} />
         <div className="App">
-          testing
+        <button className="g-recaptcha" 
+          data-sitekey="6Lf_u84nAAAAAAnNuPWpkfjdbRT3dLi28Nt-zMuz" 
+          data-callback='onSubmit' 
+          data-action='submit'
+          onSubmit={()=> {console.log('true submit')}}
+          
+          >Submit</button>
         </div>
       </GoogleReCaptchaProvider>
     </div>
