013e29ea44bef79aad61965820c25b58b82b1e25
Ramdin Sharvesh <ramdin.s@pg.com>
2023-08-28 01:11:33 +0400

    add firebase config and initialise with ENT recaptcha

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.css';
 import { GoogleReCaptchaProvider, GoogleReCaptcha, useGoogleReCaptcha } from 'react-google-recaptcha-v3';
+import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 function App() {
+  // TODO: Replace the following with your app's Firebase project configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBfhpK_TFSY6PNtWg8EjaJSjgIkI_GGey4",
+    authDomain: "recaptchav3-61eb1.firebaseapp.com",
+    projectId: "recaptchav3-61eb1",
+    storageBucket: "recaptchav3-61eb1.appspot.com",
+    messagingSenderId: "46461743954",
+    appId: "1:46461743954:web:6938f44e60fb5d04e6808b",
+    measurementId: "G-EP3WJ3ST65"
+  };
+  const app = initializeApp(firebaseConfig);
   const YourReCaptchaComponent = () => {
     const { executeRecaptcha } = useGoogleReCaptcha();
     // Create an event handler so you can call the verification on button click event or form submit
@@ -14,6 +27,17 @@ function App() {
   
       const token = await executeRecaptcha('submit');
       console.log('token received', token);
+      
+
+  // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+    // key is the counterpart to the secret key you set in the Firebase console.
+    initializeAppCheck(app, {
+      provider: new ReCaptchaV3Provider('6Lcm7NwnAAAAALwID8kRKnkWhhAxMUSlO5PasZFk'),
+
+      // Optional argument. If true, the SDK automatically refreshes App Check
+      // tokens as needed.
+      isTokenAutoRefreshEnabled: true
+    });
       // Do whatever you want with the token
     }, [executeRecaptcha]);
   
@@ -37,7 +61,7 @@ function App() {
           <input type="hidden" name="submit_frm" value="1"/>
 
           <button className="g-recaptcha" 
-            data-sitekey="6Lf_u84nAAAAAAnNuPWpkfjdbRT3dLi28Nt-zMuz" 
+            data-sitekey="6Lcm7NwnAAAAALwID8kRKnkWhhAxMUSlO5PasZFk" 
             data-callback='onSubmit' 
             data-action='submit'
             onClick={handleReCaptchaVerify}
@@ -50,7 +74,7 @@ function App() {
 
   return (
     <div className="App">
-      <GoogleReCaptchaProvider reCaptchaKey="6Lf_u84nAAAAAAnNuPWpkfjdbRT3dLi28Nt-zMuz">
+      <GoogleReCaptchaProvider reCaptchaKey="6Lcm7NwnAAAAALwID8kRKnkWhhAxMUSlO5PasZFk">
         <YourReCaptchaComponent />        
       </GoogleReCaptchaProvider>
     </div>
