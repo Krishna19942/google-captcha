352ed79849248f03ce5636c3ba0434b2b7e732e8
Ramdin Sharvesh <ramdin.s@pg.com>
2023-08-28 14:50:30 +0400

    add axios package and post token to siteverify

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,22 @@ import './App.css';
 import { GoogleReCaptchaProvider, GoogleReCaptcha, useGoogleReCaptcha } from 'react-google-recaptcha-v3';
 import { initializeApp } from "firebase/app";
 import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+import axios from 'axios';
 
 function App() {
   // TODO: Replace the following with your app's Firebase project configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyBfhpK_TFSY6PNtWg8EjaJSjgIkI_GGey4",
-    authDomain: "recaptchav3-61eb1.firebaseapp.com",
-    projectId: "recaptchav3-61eb1",
-    storageBucket: "recaptchav3-61eb1.appspot.com",
-    messagingSenderId: "46461743954",
-    appId: "1:46461743954:web:6938f44e60fb5d04e6808b",
-    measurementId: "G-EP3WJ3ST65"
-  };
-  const app = initializeApp(firebaseConfig);
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyBfhpK_TFSY6PNtWg8EjaJSjgIkI_GGey4",
+  //   authDomain: "recaptchav3-61eb1.firebaseapp.com",
+  //   projectId: "recaptchav3-61eb1",
+  //   storageBucket: "recaptchav3-61eb1.appspot.com",
+  //   messagingSenderId: "46461743954",
+  //   appId: "1:46461743954:web:6938f44e60fb5d04e6808b",
+  //   measurementId: "G-EP3WJ3ST65"
+  // };
+  // const app = initializeApp(firebaseConfig);
+
+
   const YourReCaptchaComponent = () => {
     const { executeRecaptcha } = useGoogleReCaptcha();
     // Create an event handler so you can call the verification on button click event or form submit
@@ -27,17 +30,31 @@ function App() {
   
       const token = await executeRecaptcha('submit');
       console.log('token received', token);
+
+      // Call Google's API to get score
+      const res = await axios.post(
+        `https://www.google.com/recaptcha/api/siteverify?secret=6Lcm7NwnAAAAACW9_sCcNU874f_QFlglX9RTdIvR&response=${token}`
+      );
+      
+      // Extract result from the API response
+      if (res.data.success){
+        console.log('res',res)
+        console.log('Valid');
+      } else {
+        console.log('res',res)
+        console.log('Invalid');
+      }
       
 
   // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
     // key is the counterpart to the secret key you set in the Firebase console.
-    initializeAppCheck(app, {
-      provider: new ReCaptchaV3Provider('6Lcm7NwnAAAAALwID8kRKnkWhhAxMUSlO5PasZFk'),
+    // initializeAppCheck(app, {
+    //   provider: new ReCaptchaV3Provider('6Lcm7NwnAAAAALwID8kRKnkWhhAxMUSlO5PasZFk'),
 
-      // Optional argument. If true, the SDK automatically refreshes App Check
-      // tokens as needed.
-      isTokenAutoRefreshEnabled: true
-    });
+    //   // Optional argument. If true, the SDK automatically refreshes App Check
+    //   // tokens as needed.
+    //   isTokenAutoRefreshEnabled: true
+    // });
       // Do whatever you want with the token
     }, [executeRecaptcha]);
   
